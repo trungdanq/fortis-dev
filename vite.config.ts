06248d72bf79cf5d92c6e798832773f79cb608f6
Trung Dang Quoc <trung.dangquoc@wsc-sports.com>
2025-08-29 17:02:56 +0700

    fix typo

--- vite.config.ts
+++ vite.config.ts
@@ -10,7 +10,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
     },
   },
-  base: process.env.NODE_ENV === 'production' ? '/repository-name/' : '/',
+  base: process.env.NODE_ENV === 'production' ? '/fortis-dev/' : '/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
